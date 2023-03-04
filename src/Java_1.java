b73fcc43381bae1ed7734036d4c44c785dc86aec
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-05 00:28:55 +0530

    EqualityOfArray

---
+++ src/Java_1.java
@@ -0,0 +1,15 @@
+public class Java_1 {
+    public static void main(String[]args){
+        System.out.println("Namaste World");   //This is a printing statement
+//        These are the datatypes of the java
+        byte a;
+        int b;
+        float c;
+        long d;
+        char e;
+        double f;
+        boolean g;
+        short h;
+
+    }
+}
