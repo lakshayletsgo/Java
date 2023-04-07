4a500e62dac8824e3c4f188a949d716921c25227
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-07 13:16:23 +0530

    File Handling in Java

---
+++ src/Java_47.java
@@ -0,0 +1,52 @@
+//File Handling in Java
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+public class Java_47 {
+    public static void main(String[] args) {
+//        This is used to create a new file
+//        File f1 = new File("NewFile.txt");
+//        try {
+//            f1.createNewFile();
+//        }catch (Exception e){
+//            System.out.println(e);
+//            e.printStackTrace();
+//        }
+
+
+//        This is used to write on a file
+//        Closing a file is very important otherwise sometimes it wont write on the file
+//        try {
+//
+//            FileWriter fileWriter = new FileWriter("NewFile.txt");
+//            fileWriter.write("This is the new File \n this is the write function");
+//            fileWriter.close();
+//        }
+//    catch (Exception e) {
+//        System.out.println(e);
+//    }
+
+
+//        This is used to read a file
+//        File myfile = new File("NewFile.txt");
+//        try {
+//            Scanner sc = new Scanner(myfile);
+//            while (sc.hasNextLine()){
+//                String sc1 = sc.nextLine();
+//                System.out.println(sc1);
+//            }
+//            sc.close();
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
+
+//        This is used to delete the content of the file
+//        File myfile = new File("NewFile.txt");
+//        if(myfile.delete()){
+//            System.out.println("The file has been deleted successfully and the name of the file is "+myfile.getName());
+//        }else {
+//            System.out.println("Some Error Occured!!!!!!!");
+//        }
+
+    }
+}
