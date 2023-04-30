641c1cb500250217c9bdd0eef4ded2c6354d5825
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-04-30 21:36:57 +0530

    FOCP_Project

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import LoginPage.LoginPage;
 import ExamCreation.*;
+import OnlineExam.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         LoginPage.main();
-        ExamCreation_MCQ.main();
-        ExamCreation_Subjective.main();
-        ExamCreation_Objective.main();
+        OnlineExam.main();
+        ExamTaking.main();
     }
 }
