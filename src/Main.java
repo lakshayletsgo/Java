1e64a0e38f03ef7be266dbc546dada927164ed0e
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-07-26 22:57:25 +0530

    Exam Preparation

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
-import LoginPage.LoginPage;
-import ExamCreation.*;
-import OnlineExam.*;
-
-import java.util.Scanner;
-import Grading.*;
-import LoginPage.*;
-
-public class Main {
-    public static void main(String[] args) {
-        LoginPage.main();
-        System.out.println("Enter your role:\n 1.Teacher\t2.Student \n");
-        Scanner sc = new Scanner(System.in);
-        int role = sc.nextInt();
-        if(role==1){
-        OnlineExam.main();
-        System.out.println("To check the exam press 1 ");
-        int test = sc.nextInt();
-        if(test==1){
-            ExamTaking.main();
-        }
-        }else{
-        ExamTaking.main();
-        System.out.println("Press 1 to get your report");
-        int r = sc.nextInt();
-        if(r==1){
-
-            Grading2.main();
-        }else {
-            return;
-        }
-        }
-
-    }
-}
+//import LoginPage.LoginPage;
+//import ExamCreation.*;
+//import OnlineExam.*;
+//
+//import java.util.Scanner;
+//import Grading.*;
+//import LoginPage.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        LoginPage.main();
+//        System.out.println("Enter your role:\n 1.Teacher\t2.Student \n");
+//        Scanner sc = new Scanner(System.in);
+//        int role = sc.nextInt();
+//        if(role==1){
+//        OnlineExam.main();
+//        System.out.println("To check the exam press 1 ");
+//        int test = sc.nextInt();
+//        if(test==1){
+//            ExamTaking.main();
+//        }
+//        }else{
+//        ExamTaking.main();
+//        System.out.println("Press 1 to get your report");
+//        int r = sc.nextInt();
+//        if(r==1){
+//
+//            Grading2.main();
+//        }else {
+//            return;
+//        }
+//        }
+//
+//    }
+//}
