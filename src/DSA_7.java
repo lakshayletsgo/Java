76778e833f17a9045a7ddf57400bb1f20025b274
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-09-01 19:56:21 +0530

    Peak Element in a Array

--- src/DSA_7.java
+++ src/DSA_7.java
@@ -1,14 +1,14 @@
 public class DSA_7 {
     public static void main(String[] args) {
         int [] arr = {2,5,10,2,1};
-        System.out.println(peakIndexInMountain(arr));
+        System.out.println(findPeakElement(arr));
     }
-    static int peakIndexInMountain(int[]arr){
+    static int findPeakElement(int[]nums){
         int start =0;
-        int end = arr.length-1;
+        int end = nums.length-1;
         while(start<end){
             int mid = start+(end-start)/2;
-            if(arr[mid]>arr[mid+1]){
+            if(nums[mid]>nums[mid+1]){
                 end =mid;
             }else{
                 start = mid+1;
