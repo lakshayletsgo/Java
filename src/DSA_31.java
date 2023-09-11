0315b3cb0fe0abc1e000731a3969eba1ee7b3191
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-09-12 00:33:37 +0530

    Dice Combination

---
+++ src/DSA_31.java
@@ -0,0 +1,15 @@
+//Dice Combination
+public class DSA_31 {
+    public static void main(String[] args) {
+        dice("",4);
+    }
+    static void dice(String p,int target){
+        if(target==0){
+            System.out.println(p);
+            return;
+        }
+        for(int i=1;i<=6&&i<=target;i++){
+            dice(p+i,target-i);
+        }
+    }
+}
