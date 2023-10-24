30a5f941cb84b03731bf2382222917ca04fcfb46
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-10-24 13:33:16 +0530

    average-of-levels-in-binary-tree

---
+++ src/LeetCode_40.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeetCode_40 {
+    public static void main(String[] args) {
+//        System.out.println();
+    }
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode > queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            double avg = 0;
+            int levelSize = queue.size();
+            for(int i=0;i<levelSize;i++){
+                TreeNode currentNode=  queue.poll();
+                avg+=currentNode.val;
+                if(currentNode.left!=null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            avg/=levelSize;
+            ans.add(avg);
+
+
+        }
+        return ans;
+
+    }
+}
+
