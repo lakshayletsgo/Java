1635f5453f41ab81fe15be29ad4c9e01457d4fd6
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-11-10 23:27:58 +0530

    Traversal In Binary Tree

--- src/DSA_53.java
+++ src/DSA_53.java
@@ -1,31 +1,32 @@
-//Traversal In Binary Tree
+////Traversal In Binary Tree
+// These are DFS traversal in Binary Tree
 
-public class DSA_53 {
-    public void preOrder(Node node){
-        if(node==null){
-            return;
-        }
-        System.out.println(node.value+" ");
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-    public void inOrder(Node node){
-        if(node==null){
-            return;
-        }
-        preOrder(node.left);
-        System.out.println(node.value+" ");
-        preOrder(node.right);
-    }
-    public void postOrder(Node node){
-        if(node==null){
-            return;
-        }
-        preOrder(node.left);
-        preOrder(node.right);
-        System.out.println(node.value+" ");
-    }
-    public static void main(String[] args) {
-
-    }
-}
+//public class DSA_53 {
+//    public void preOrder(Node node){
+//        if(node==null){
+//            return;
+//        }
+//        System.out.println(node.value+" ");
+//        preOrder(node.left);
+//        preOrder(node.right);
+//    }
+//    public void inOrder(Node node){
+//        if(node==null){
+//            return;
+//        }
+//        preOrder(node.left);
+//        System.out.println(node.value+" ");
+//        preOrder(node.right);
+//    }
+//    public void postOrder(Node node){
+//        if(node==null){
+//            return;
+//        }
+//        preOrder(node.left);
+//        preOrder(node.right);
+//        System.out.println(node.value+" ");
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+//}
