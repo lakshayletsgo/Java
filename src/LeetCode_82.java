c9b5204e4876b4b657ecd143e0742f099ef5c969
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-11-16 22:27:29 +0530

    longest-even-odd-subarray-with-threshold

---
+++ src/LeetCode_82.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/longest-even-odd-subarray-with-threshold/
+public class LeetCode_82 {
+    public static void main(String[] args) {
+        int[]arr = {2,3,4,1,5};
+        System.out.println(longestAlternatingSubarray(arr,4));
+    }
+    public static int longestAlternatingSubarray(int[] nums, int th) {
+
+        int n = nums.length,maxi = 0,i = 0,j = 0,flag = 0;
+        while(j < n){
+            if(flag == 0){
+                if(nums[j] % 2 == 0 && nums[j] <= th){
+                    i = j;
+                    maxi = Math.max(maxi,j - i + 1);
+                    flag = 1;
+                }
+            }
+            else if(flag == 1){
+                int x = nums[j-1],y = nums[j],c = x + y;
+                if(c % 2 != 0 && nums[j] <= th)
+                    maxi = Math.max(maxi,j - i + 1);
+                else{
+                    flag = 0;
+                    j--;
+                }
+            }
+            j++;
+        }
+        return maxi;
+
+    }
+}
