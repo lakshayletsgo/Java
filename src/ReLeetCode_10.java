a54d4b82e129d1ed94a823034f337d20268f3670
lakshay <lakshay22csu100@ncuindia.edu>
2024-11-01 12:42:57 +0530

    Constant Space two-sum-ii-input-array-is-sorted

--- src/ReLeetCode_10.java
+++ src/ReLeetCode_10.java
@@ -5,22 +5,40 @@ public class ReLeetCode_10 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2,3,4},6)));
     }
-    public static int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i=0;i<numbers.length;i++){
-            map.put(numbers[i],i);
+//    public static int[] twoSum(int[] numbers, int target) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int i=0;i<numbers.length;i++){
+//            map.put(numbers[i],i);
+//
+//        }
+//        int i=0;
+//        int j= numbers.length-1;
+//        while (i<j){
+//            if (map.containsKey(target-numbers[i])){
+//                return new int[]{i,map.get(target-numbers[i])};
+//            }
+//            i++;
+//        }
+//
+//
+//        return new int[]{-1,-1};
+//    }
 
-        }
-        int i=0;
-        int j= numbers.length-1;
-        while (i<j){
-            if (map.containsKey(target-numbers[i])){
-                return new int[]{i,map.get(target-numbers[i])};
+//    Constant Space
+public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+        while (left<right){
+            int total = numbers[left]+numbers[right];
+            if(total==target){
+                return new int[]{left+1,right+1};
+
+            } else if (total<target) {
+                left++;
+            }else{
+                right--;
             }
-            i++;
         }
-
-
         return new int[]{-1,-1};
-    }
+}
 }
