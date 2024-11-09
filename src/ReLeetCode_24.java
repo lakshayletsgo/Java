f3c708d09ea494a485c867496f38fe96e367a761
lakshay <lakshay22csu100@ncuindia.edu>
2024-11-09 17:51:54 +0530

    ReLeetCode_23 find-minimum-in-rotated-sorted-array

---
+++ src/ReLeetCode_24.java
@@ -0,0 +1,18 @@
+public class ReLeetCode_24 {
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{4,5,6,7,0,1,2}));
+    }
+    public  static  int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        while (left<right){
+            int mid = left + (right-left)/2;
+             if (nums[mid] <nums[right]) {
+                right = mid;
+            }else{
+               left = mid+1;
+            }
+        }
+        return nums[left];
+    }
+}
