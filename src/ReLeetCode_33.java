392dd3daa99a8437516e5dd49949b21f33dd23b8
lakshay <lakshay22csu100@ncuindia.edu>
2024-11-20 23:28:30 +0530

    ReLeetCode_33 sliding-window-maximum

---
+++ src/ReLeetCode_33.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReLeetCode_33 {
+    public static void main(String[] args) {
+
+    }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            int num = nums[idx];
+
+            while (!deque.isEmpty() && deque.getLast() < num) {
+                deque.pollLast();
+            }
+            deque.addLast(num);
+
+            if (idx >= k && nums[idx - k] == deque.getFirst()) {
+                deque.pollFirst();
+            }
+
+            if (idx >= k - 1) {
+                res.add(deque.getFirst());
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+}
