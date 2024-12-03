95d915ac8670d44ccb17818df4b376b89f695138
lakshay <lakshay22csu100@ncuindia.edu>
2024-12-03 23:56:44 +0530

    ReLeetCode_45 /invert-binary-tree

---
+++ src/ReLeetCode_45.java
@@ -0,0 +1,32 @@
+
+public class ReLeetCode_45 {
+    private class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public static void main(String[] args) {
+
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
