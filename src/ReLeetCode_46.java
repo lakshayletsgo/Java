5fa90fed555923c4969ac3deabfce3128037a1e1
lakshay <lakshay22csu100@ncuindia.edu>
2024-12-04 23:41:42 +0530

    ReLeetCode_45 maximum-depth-of-binary-tree

---
+++ src/ReLeetCode_46.java
@@ -0,0 +1,22 @@
+public class ReLeetCode_46 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }}
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
