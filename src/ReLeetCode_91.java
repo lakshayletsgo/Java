a487fef9b4fc8e6225c750e63328ac71fb562477
lakshay <lakshay22csu100@ncuindia.edu>
2025-02-08 18:35:19 +0530

    ReLeetCode_91 binary-tree-cameras

--- src/ReLeetCode_91.java
+++ src/ReLeetCode_91.java
@@ -4,27 +4,20 @@ public class ReLeetCode_91 {
     }
     private int numOfCameras = 0;
 
-    public int minCameraCover(TreeNode root) {
-        return dfs(root) == -1 ? numOfCameras + 1 : numOfCameras;
+    public int minCameraCover(TreeNode root){
+        return dfs(root)==-1?numOfCameras+1:numOfCameras;
     }
 
-    // -1: NOT MONITORED
-    //  0: MONITORED
-    //  1: HAS CAMERA
     private int dfs(TreeNode root) {
-        if (root == null) return 0;
-
+        if (root==null)return 0;
         int left = dfs(root.left);
         int right = dfs(root.right);
-
-        if (left == -1 || right == -1) {
+        if (left==-1||right==-1){
             numOfCameras++;
             return 1;
         }
-
-        if (left == 1 || right == 1)
-            return 0;
-
+        if (left==1||right==1)return 0;
         return -1;
     }
+
 }
