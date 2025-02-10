8fdbb3ff0155f87cf3428d496645f99728a94259
lakshay <lakshay22csu100@ncuindia.edu>
2025-02-10 23:18:25 +0530

    ReLeetCode_83 binary-tree-maximum-path-sum

--- src/ReLeetCode_83.java
+++ src/ReLeetCode_83.java
@@ -9,14 +9,15 @@ public class ReLeetCode_83 {
     }
 
     private int helper(TreeNode root) {
-        if (root==null) return 0;
+        if (root==null)return 0;
         int left = helper(root.left);
         int right = helper(root.right);
         left = Math.max(left,0);
         right = Math.max(right,0);
-        int nodeSum  = root.val+left+right;
-        int leftRightSum = root.val+Math.max(left,right);
-        globalMax = Math.max(nodeSum,globalMax);
-        return leftRightSum;
+        int currNodeSum = root.val+left+right;
+        int currMax = root.val+Math.max(left,right);
+        globalMax = Math.max(currNodeSum,globalMax);
+        return currMax;
     }
+
 }
