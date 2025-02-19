bd5755c7f6f89d9f390a1e4c202c4aae9604dd5e
lakshay <lakshay22csu100@ncuindia.edu>
2025-02-19 18:45:57 +0530

    ReLeetCode_102 design-twitter

--- src/ReLeetCode_102.java
+++ src/ReLeetCode_102.java
@@ -4,44 +4,83 @@ public class ReLeetCode_102 {
     public static void main(String[] args) {
 
     }
-    Map<Integer, Set<Integer>> fans = new HashMap<>();
-    Map<Integer, LinkedList<Tweet>> tweets = new HashMap<>();
-    int cnt = 0;
-
-    public void postTweet(int userId, int tweetId) {
-        if (!fans.containsKey(userId)) fans.put(userId, new HashSet<>());
-        fans.get(userId).add(userId);
-        if (!tweets.containsKey(userId)) tweets.put(userId, new LinkedList<>());
-        tweets.get(userId).addFirst(new Tweet(cnt++, tweetId));
+    private static int timeStamp =0;
+    private Map<Integer,User> userMap;
+    private class Tweet{
+        public int time ;
+        public int id ;
+        public Tweet next ;
+        public Tweet(int id){
+            this.id = id;
+            next = null;
+            time = timeStamp++;
+        }
+    }
+    private class User{
+        public int id;
+        public Set<Integer> followed;
+        public Tweet tweet_head;
+        public User(int id) {
+            this.id = id;
+            followed = new HashSet<>();
+            tweet_head = null;
+            follow(id);
+        }
+        public void follow(int id) {followed.add(id);}
+        public void unfollow(int id) {followed.remove(id);}
+        public void post(int id) {
+            Tweet t = new Tweet(id);
+            t.next = tweet_head;
+            tweet_head = t;
+        }
+    }
+    public ReLeetCode_102() {
+        userMap = new HashMap<>();
     }
 
+    public void postTweet(int userId, int tweetId) {
+        if (!userMap.containsKey(userId)){
+            User u = new User(userId);
+            userMap.put(userId,u);
+        }
+        userMap.get(userId).post(tweetId);
+    }
+
+
     public List<Integer> getNewsFeed(int userId) {
-        if (!fans.containsKey(userId)) return new LinkedList<>();
-        PriorityQueue<Tweet> feed = new PriorityQueue<>((t1, t2) -> t2.time - t1.time);
-        fans.get(userId).stream()
-                .filter(f -> tweets.containsKey(f))
-                .forEach(f -> tweets.get(f).forEach(feed::add));
-        List<Integer> res = new LinkedList<>();
-        while (feed.size() > 0 && res.size() < 10) res.add(feed.poll().id);
-        return res;
+        List<Integer> list = new LinkedList<>();
+        if (!userMap.containsKey(userId)) {return list;}
+        Set<Integer> users=  userMap.get(userId).followed;
+        PriorityQueue<Tweet> q = new PriorityQueue<Tweet>(users.size(),(a,b)->(b.time-a.time));
+        for (int user:users){
+            Tweet t = userMap.get(user).tweet_head;
+            if (t!=null)q.add(t);
+        }
+        int n=0;
+        while (!q.isEmpty()&&n<10){
+            Tweet t = q.poll();
+            list.add(t.id);
+            n++;
+            if (t.next!=null)q.add(t.next);
+        }
+        return list;
     }
 
     public void follow(int followerId, int followeeId) {
-        if (!fans.containsKey(followerId)) fans.put(followerId, new HashSet<>());
-        fans.get(followerId).add(followeeId);
+        if (!userMap.containsKey(followerId)){
+            User u = new User(followerId);
+            userMap.put(followerId,u);
+        }
+        if (!userMap.containsKey(followeeId)){
+            User u = new User(followeeId);
+            userMap.put(followeeId,u);
+        }
+        userMap.get(followerId).follow(followeeId);
     }
 
     public void unfollow(int followerId, int followeeId) {
-        if (fans.containsKey(followerId) && followeeId != followerId) fans.get(followerId).remove(followeeId);
+        if (!userMap.containsKey(followerId)||followerId==followeeId)return;
+        userMap.get(followerId).unfollow(followeeId);
     }
 
-    class Tweet {
-        int time;
-        int id;
-
-        Tweet(int time, int id) {
-            this.time = time;
-            this.id = id;
-        }
-    }
 }
