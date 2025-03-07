ed2c63ed0c6423825db44a9ec6ab00b111da2a22
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-07 18:25:51 +0530

    ReLeetCode_123 merge-intervals

---
+++ src/ReLeetCode_123.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReLeetCode_123 {
+    public static void main(String[] args) {
+
+    }
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
+            }
+        }
+
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
