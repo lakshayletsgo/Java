ce28029485862bcb8f3eb488e76fb1d08ccc1f72
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-08 20:10:20 +0530

    ReLeetCode_124 non-overlapping-intervals

---
+++ src/ReLeetCode_124.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ReLeetCode_124 {
+    public static void main(String[] args) {
+
+    }
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int res = 0;
+
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int prev_end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (prev_end > intervals[i][0]) {
+                res++;
+            } else {
+                prev_end = intervals[i][1];
+            }
+        }
+
+        return res;
+    }
+}
