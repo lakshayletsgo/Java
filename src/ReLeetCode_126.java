e40237bdecc5d0cfd7e7bea4568359a7bb36bbd0
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-11 23:53:53 +0530

    ReLeetCode_126 maximum-subarray

---
+++ src/ReLeetCode_126.java
@@ -0,0 +1,18 @@
+public class ReLeetCode_126 {
+    public static void main(String[] args) {
+
+    }
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+
+            if(sum<0) sum = 0;
+        }
+
+        return max;
+    }
+}
