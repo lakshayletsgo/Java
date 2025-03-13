e76c35e489c52315273deea339370a8440a7b4f0
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-13 14:53:04 +0530

    ReLeetCode_128 jump-game-ii

--- src/ReLeetCode_128.java
+++ src/ReLeetCode_128.java
@@ -2,5 +2,17 @@ public class ReLeetCode_128 {
     public static void main(String[] args) {
 
     }
-
+    public int jump(int[] nums) {
+        int far =0,jump=0,near=0;
+        while (far<nums.length-1){
+            int farest = 0;
+            for (int i=near;i<=far;i++){
+                farest = Math.max(farest,i+nums[i]);
+            }
+            near = far+1;
+            far = farest;
+            jump++;
+        }
+        return jump;
+    }
 }
