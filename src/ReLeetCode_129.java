37ae9429491d4e7833c08b894a1f1edcf71324be
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-14 20:40:05 +0530

    ReLeetCode_129 longest-palindromic-substring

---
+++ src/ReLeetCode_129.java
@@ -0,0 +1,20 @@
+public class ReLeetCode_129 {
+    public static void main(String[] args) {
+
+    }
+    public String longestPalindrome(String s) {
+        int n = s.length(),start = 0,end= 0;
+        boolean[][]dp = new boolean[n][n];
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j+i<n; j++) {
+                dp[j][i+j] =s.charAt(j)==s.charAt(i+j)&&(i<2||dp[j+1][i+j-1]);
+                if (dp[j][i+j]&&i>end-start){
+                    start = j;
+                    end = i+j;
+                }
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+}
