f25dffa0912ac93ee3d646c4d7c46a01bfa2f0e3
lakshay <lakshay22csu100@ncuindia.edu>
2025-03-20 12:31:19 +0530

    ReLeetCode_141 coin-change

---
+++ src/ReLeetCode_141.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ReLeetCode_141 {
+    public static void main(String[] args) {
+
+    }
+    public int coinChange(int[] coins, int amount) {
+        int []dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int coin:coins) for (int i=coin;i<=amount;i++) dp[i] = Math.min(dp[i],dp[i-coin]+1);
+        return dp[amount]>amount?-1:dp[amount];
+    }
+
+}
