f5c94603b7387bff99459c0892ccfcb3d2cf5ede
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-05 13:17:41 +0530

    ReLeetCode_163 reverse-bits

---
+++ src/ReLeetCode_163.java
@@ -0,0 +1,14 @@
+public class ReLeetCode_163 {
+    public static void main(String[] args) {
+
+    }
+    public int reverseBits(int n) {
+        int result=0;
+        for (int i = 0; i < 32; i++) {
+            result<<=1;
+            result|=(n&1);
+            n>>=1;
+        }
+        return result;
+    }
+}
