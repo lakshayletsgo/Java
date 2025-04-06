d1eb8a772a9fce17bbc306eb8b6824f22790260e
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-06 12:11:02 +0530

    ReLeetCode_166 reverse-integer

---
+++ src/ReLeetCode_166.java
@@ -0,0 +1,13 @@
+public class ReLeetCode_166 {
+    public static void main(String[] args) {
+
+    }
+    public int reverse(int x) {
+        long ans=0;
+        while (x!=0){
+            ans = ans*10+ x%10;
+            x/=10;
+        }
+        return (ans<Integer.MIN_VALUE||ans>Integer.MAX_VALUE)?0:(int)ans;
+    }
+}
