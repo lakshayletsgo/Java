21ca06293ecd288c667029f91c087ad68aabacf6
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-07 16:15:51 +0530

    ReLeetCode_167 rotate-image

---
+++ src/ReLeetCode_167.java
@@ -0,0 +1,22 @@
+public class ReLeetCode_167 {
+    public static void main(String[] args) {
+
+    }
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                int temp= matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n/2;j++){
+                int temp=matrix[i][n-1-j];
+                matrix[i][n-1-j]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
+    }
+}
