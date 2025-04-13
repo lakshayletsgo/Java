5cd2be5f89f06d234204def8566d14d22b6855fd
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-13 13:56:54 +0530

    PracticeReLeetCode max-area

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -63,24 +63,37 @@ public class PracticeReLeetCode {
 
 
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        Arrays.sort(nums);
-        Map<Integer,Integer> map =new HashMap<>();
-        for(int i=0;i<nums.length;i++)map.put(nums[i],i);
-        for(int i=0;i<nums.length;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
-            int newTarget=-nums[i];
-            int start=i+1;
-            int end = nums.length-1;
-            while(start<end&&start!=i&&end!=i){
-                if(nums[start]+nums[end]==newTarget){
-                    ans.add(Arrays.asList(nums[i],nums[start],nums[end]));
-                    start++;
-                    while(nums[start]==nums[start-1]&&start<end)start++;
-                }else if(nums[start]+nums[end]<newTarget)start++;
-                else end--;
-            }
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> ans= new ArrayList<>();
+//        Arrays.sort(nums);
+//        Map<Integer,Integer> map =new HashMap<>();
+//        for(int i=0;i<nums.length;i++)map.put(nums[i],i);
+//        for(int i=0;i<nums.length;i++){
+//            if(i>0&&nums[i]==nums[i-1])continue;
+//            int newTarget=-nums[i];
+//            int start=i+1;
+//            int end = nums.length-1;
+//            while(start<end&&start!=i&&end!=i){
+//                if(nums[start]+nums[end]==newTarget){
+//                    ans.add(Arrays.asList(nums[i],nums[start],nums[end]));
+//                    start++;
+//                    while(nums[start]==nums[start-1]&&start<end)start++;
+//                }else if(nums[start]+nums[end]<newTarget)start++;
+//                else end--;
+//            }
+//        }
+//        return ans;}
+
+
+    public int maxArea(int[] height) {
+        int s=0;
+        int max=0;
+        int e=height.length-1;
+        while(s<e){
+            max=Math.max(Math.min(height[s],height[e])*(e-s),max);
+            if(height[s]>height[e])e--;
+            else s++;
         }
-        return ans;}
+        return max;
+    }
 }
