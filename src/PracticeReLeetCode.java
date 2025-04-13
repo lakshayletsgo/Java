01596deba52c6d3f392a545ad60544844eed5706
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-13 13:37:02 +0530

    PracticeReLeetCode three-sum

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -54,10 +54,33 @@ public class PracticeReLeetCode {
 
 
 
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map= new HashMap<>();
+//    public int[] twoSum(int[] nums, int target) {
+//        Map<Integer,Integer> map= new HashMap<>();
+//        for(int i=0;i<nums.length;i++)map.put(nums[i],i);
+//        for(int i=0;i<nums.length;i++)if(map.containsKey(target-nums[i])&&map.get(target-nums[i])!=i)return new int[]{i,map.get(target-nums[i])};
+//        return new int[]{-1,-1};
+//    }
+
+
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        Arrays.sort(nums);
+        Map<Integer,Integer> map =new HashMap<>();
         for(int i=0;i<nums.length;i++)map.put(nums[i],i);
-        for(int i=0;i<nums.length;i++)if(map.containsKey(target-nums[i])&&map.get(target-nums[i])!=i)return new int[]{i,map.get(target-nums[i])};
-        return new int[]{-1,-1};
-    }
+        for(int i=0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1])continue;
+            int newTarget=-nums[i];
+            int start=i+1;
+            int end = nums.length-1;
+            while(start<end&&start!=i&&end!=i){
+                if(nums[start]+nums[end]==newTarget){
+                    ans.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                    start++;
+                    while(nums[start]==nums[start-1]&&start<end)start++;
+                }else if(nums[start]+nums[end]<newTarget)start++;
+                else end--;
+            }
+        }
+        return ans;}
 }
