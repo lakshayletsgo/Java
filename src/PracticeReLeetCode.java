f945c3b426e7e92e2ed918fff3b84d3964222885
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-14 23:22:23 +0530

    PracticeReLeetCode maxArea

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -85,15 +85,15 @@ public class PracticeReLeetCode {
 //        return ans;}
 
 
-//    public int maxArea(int[] height) {
-//        int s=0;
-//        int max=0;
-//        int e=height.length-1;
-//        while(s<e){
-//            max=Math.max(Math.min(height[s],height[e])*(e-s),max);
-//            if(height[s]>height[e])e--;
-//            else s++;
-//        }
-//        return max;
-//    }
+    public int maxArea(int[] height) {
+        int s=0;
+        int max=0;
+        int e=height.length-1;
+        while(s<e){
+            max=Math.max(Math.min(height[s],height[e])*(e-s),max);
+            if(height[s]>height[e])e--;
+            else s++;
+        }
+        return max;
+    }
 }
