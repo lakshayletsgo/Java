9cc78d3ce57b4ce324d861e856a61d76792dede5
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-22 12:05:30 +0530

    PracticeReLeetCode largest-rectangle-in-histogram

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -242,20 +242,20 @@ public class PracticeReLeetCode {
 //    }
 
 
-    public int largestRectangleArea(int[] heights) {
-        Stack<Map.Entry<Integer,Integer>> stack = new Stack<>();
-        int n= heights.length;
-        int maxArea=0;
-        for(int i=0;i<=n;i++){
-            int start= i;
-            while((!stack.isEmpty())&&(i==n||stack.peek().getValue()>heights[i])){
-                Map.Entry<Integer,Integer> temp=stack.pop();
-                int length= i-temp.getKey();
-                maxArea= Math.max(maxArea,temp.getValue()*length);
-                start=temp.getKey();
-            }
-            if(i<n)stack.push(Map.entry(start,heights[i]));
-        }
-        return maxArea;
-    }
+//    public int largestRectangleArea(int[] heights) {
+//        Stack<Map.Entry<Integer,Integer>> stack = new Stack<>();
+//        int n= heights.length;
+//        int maxArea=0;
+//        for(int i=0;i<=n;i++){
+//            int start= i;
+//            while((!stack.isEmpty())&&(i==n||stack.peek().getValue()>heights[i])){
+//                Map.Entry<Integer,Integer> temp=stack.pop();
+//                int length= i-temp.getKey();
+//                maxArea= Math.max(maxArea,temp.getValue()*length);
+//                start=temp.getKey();
+//            }
+//            if(i<n)stack.push(Map.entry(start,heights[i]));
+//        }
+//        return maxArea;
+//    }
 }
