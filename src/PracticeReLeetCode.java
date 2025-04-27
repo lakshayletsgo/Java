72e9af70f818fc2efa7ccf10e6372bec510dec65
lakshay <lakshay22csu100@ncuindia.edu>
2025-04-27 18:12:36 +0530

    PracticeReLeetCode median-of-two-sorted-arrays

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -341,35 +341,70 @@ public class PracticeReLeetCode {
 
 
 
-    private class Data{
-        String str;
-        int time;
-        Data(String str, int time){
-            this.str=str;
-            this.time=time;
+//    private class Data{
+//        String str;
+//        int time;
+//        Data(String str, int time){
+//            this.str=str;
+//            this.time=time;
+//        }
+//    }
+//    HashMap <String,List<Data>>map;
+//
+//    public TimeMap() {
+//        map=new HashMap<>();
+//    }
+//
+//    public void set(String key, String value, int timestamp) {
+//        map.computeIfAbsent(key,k->new ArrayList<>()).add(new Data(value,timestamp));
+//    }
+//
+//    public String get(String key, int timestamp) {
+//        if(!map.containsKey(key))return "";
+//        return binarySearch(map.get(key),timestamp);
+//    }
+//    private String binarySearch(List<Data> list,int timestamp){
+//        int l=0,r=list.size()-1;
+//        while(l<r){
+//            int mid=(l+r+1)>>>1;
+//            if(list.get(mid).time>timestamp)r=mid-1;
+//            else l=mid;
+//        }
+//        return list.get(l).time<=timestamp?list.get(l).str:"";
+//    }
+
+
+
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        if(nums1.length>nums2.length){
+            int []temp=nums1;
+            nums1=nums2;
+            nums2=temp;
         }
-    }
-    HashMap <String,List<Data>>map;
+        int n =nums1.length;
+        int m =nums2.length;
+        int left = 0;
+        int right=n;
+        while(left<=right){
+            int part1=(left+right)/2;
+            int part2=(n+m+1)/2-part1;
+            int maxLeft1=part1>0?nums1[part1-1]:Integer.MIN_VALUE;
+            int maxLeft2=part2>0?nums2[part2-1]:Integer.MIN_VALUE;
+            int maxLeft=Math.max(maxLeft1,maxLeft2);
 
-    public TimeMap() {
-        map=new HashMap<>();
-    }
+            int maxRight1= part1<n?nums1[part1]:Integer.MAX_VALUE;
+            int maxRight2= part2<m?nums2[part2]:Integer.MAX_VALUE;
+            int maxRight=Math.min(maxRight1,maxRight2);
 
-    public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key,k->new ArrayList<>()).add(new Data(value,timestamp));
-    }
-
-    public String get(String key, int timestamp) {
-        if(!map.containsKey(key))return "";
-        return binarySearch(map.get(key),timestamp);
-    }
-    private String binarySearch(List<Data> list,int timestamp){
-        int l=0,r=list.size()-1;
-        while(l<r){
-            int mid=(l+r+1)>>>1;
-            if(list.get(mid).time>timestamp)r=mid-1;
-            else l=mid;
+            if(maxLeft<=maxRight){
+                if((n+m)%2==0)return (maxLeft+maxRight)/2.0;
+                else return maxLeft;
+            }else if(maxLeft1>maxRight2)right=part1-1;
+            else left= part1+1;
         }
-        return list.get(l).time<=timestamp?list.get(l).str:"";
+        return 0.0;
+
     }
 }
