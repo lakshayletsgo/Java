db09193490b1c7414971bdde2593895c24621fcd
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-05 22:02:56 +0530

    PracticeReLeetCode merge-two-sorted-lists

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -547,16 +547,64 @@ public class PracticeReLeetCode {
 //        }
 //        return ans.stream().mapToInt(i->i).toArray();}
 
-    public ListNode reverseList(ListNode head) {
-        if(head==null||head.next==null)return head;
-        ListNode next= null;
-        ListNode prev= null;
-        while(head!=null){
-            next = head.next;
-            head.next= prev;
-            prev= head;
-            head= next;
+//    public ListNode reverseList(ListNode head) {
+//        if(head==null||head.next==null)return head;
+//        ListNode next= null;
+//        ListNode prev= null;
+//        while(head!=null){
+//            next = head.next;
+//            head.next= prev;
+//            prev= head;
+//            head= next;
+//        }
+//        return prev;
+//    }
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list2==null){
+            return list1;
+        } else if (list1==null) {
+            return list2;
         }
-        return prev;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        ListNode p3 = null;
+        if (p1.val>p2.val){
+            p3= p2;
+            p2 = p2.next;
+        }else{
+            p3 = p1;
+            p1 = p1.next;
+        }
+        ListNode newHead = p3;
+        while (p1!=null&&p2!=null){
+            if (p1.val>p2.val){
+                p3.next= p2;
+                p2 = p2.next;
+            }else{
+                p3.next = p1;
+                p1 = p1.next;
+            }
+            p3 = p3.next;
+        }
+
+        if (p1!=null){
+            while (p1!=null){
+                p3.next = p1;
+                p1 = p1.next;
+                p3 = p3.next;
+            }
+        }else{
+            while (p2!=null){
+                p3.next = p2;
+                p2 = p2.next;
+                p3 = p3.next;
+            }
+        }
+
+
+
+        return newHead;
     }
 }
