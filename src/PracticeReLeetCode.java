97123f47b32dadcb39d4eb931bce35b89a47e731
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-06 19:08:48 +0530

    PracticeReLeetCode reorder-list

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -561,47 +561,89 @@ public class PracticeReLeetCode {
 //    }
 
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list2==null){
-            return list1;
-        } else if (list1==null) {
-            return list2;
-        }
-        ListNode p1 = list1;
-        ListNode p2 = list2;
-        ListNode p3 = null;
-        if (p1.val>p2.val){
-            p3= p2;
-            p2 = p2.next;
-        }else{
-            p3 = p1;
-            p1 = p1.next;
-        }
-        ListNode newHead = p3;
-        while (p1!=null&&p2!=null){
-            if (p1.val>p2.val){
-                p3.next= p2;
-                p2 = p2.next;
-            }else{
-                p3.next = p1;
-                p1 = p1.next;
-            }
-            p3 = p3.next;
-        }
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        if (list2==null){
+//            return list1;
+//        } else if (list1==null) {
+//            return list2;
+//        }
+//        ListNode p1 = list1;
+//        ListNode p2 = list2;
+//        ListNode p3 = null;
+//        if (p1.val>p2.val){
+//            p3= p2;
+//            p2 = p2.next;
+//        }else{
+//            p3 = p1;
+//            p1 = p1.next;
+//        }
+//        ListNode newHead = p3;
+//        while (p1!=null&&p2!=null){
+//            if (p1.val>p2.val){
+//                p3.next= p2;
+//                p2 = p2.next;
+//            }else{
+//                p3.next = p1;
+//                p1 = p1.next;
+//            }
+//            p3 = p3.next;
+//        }
+//
+//        if (p1!=null){
+//            while (p1!=null){
+//                p3.next = p1;
+//                p1 = p1.next;
+//                p3 = p3.next;
+//            }
+//        }else{
+//            while (p2!=null){
+//                p3.next = p2;
+//                p2 = p2.next;
+//                p3 = p3.next;
+//            }
+//        }
+//        return newHead;
+//    }
 
-        if (p1!=null){
-            while (p1!=null){
-                p3.next = p1;
-                p1 = p1.next;
-                p3 = p3.next;
-            }
-        }else{
-            while (p2!=null){
-                p3.next = p2;
-                p2 = p2.next;
-                p3 = p3.next;
-            }
+
+
+
+
+    public void reorderList(ListNode head) {
+        if(head==null||head.next==null)return;
+        ListNode mid = getMid(head);
+        ListNode midHead= reverseMid(mid);
+        mergeMid(head,midHead);
+    }
+    public ListNode getMid(ListNode head){
+        ListNode fast=head;
+        ListNode slow = head;
+        ListNode prev= slow;
+        while(fast!=null&&fast.next!=null){
+            prev= slow;
+            slow =slow.next;
+            fast= fast.next.next;
         }
-        return newHead;
+        prev.next= null;
+        return slow;
+    }
+    public ListNode reverseMid(ListNode head){
+        ListNode prev= null;
+        ListNode next= null;
+        while(head!=null){
+            next= head.next;
+            head.next= prev;
+            prev= head;
+            head= next;
+        }
+        return prev;
+    }
+    public void mergeMid(ListNode list1, ListNode list2){
+        while(list2!=null){
+            ListNode next= list1.next;
+            list1.next= list2;
+            list1= list2;
+            list2= next;
+        }
     }
 }
