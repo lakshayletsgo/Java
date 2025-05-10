f6d6befa1456ad6a49fc88881f8b5c9c03374dea
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-10 16:11:02 +0530

    PracticeReLeetCode linked-list-cycle

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -688,17 +688,30 @@ public class PracticeReLeetCode {
 
 
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return helper(l1,l2,0);
-    }
-    public ListNode helper(ListNode l1, ListNode l2, int carry){
-        if(l1==null&&l2==null&&carry==0)return null;
-        int val1 = l1==null?0:l1.val;
-        int val2 = l2==null?0:l2.val;
-        int sum  = val1+val2+carry;
-        carry=sum/10;
-        ListNode node = new ListNode(sum%10);
-        node.next= helper(l1==null?null:l1.next,l2==null?null:l2.next,carry);
-        return node;
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        return helper(l1,l2,0);
+//    }
+//    public ListNode helper(ListNode l1, ListNode l2, int carry){
+//        if(l1==null&&l2==null&&carry==0)return null;
+//        int val1 = l1==null?0:l1.val;
+//        int val2 = l2==null?0:l2.val;
+//        int sum  = val1+val2+carry;
+//        carry=sum/10;
+//        ListNode node = new ListNode(sum%10);
+//        node.next= helper(l1==null?null:l1.next,l2==null?null:l2.next,carry);
+//        return node;
+//    }
+
+
+    public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null)return false;
+        ListNode fast= head;
+        ListNode slow = head;
+        while(fast!=null&&fast.next!=null){
+            fast= fast.next.next;
+            slow = slow.next;
+            if(fast==slow)return true;
+        }
+        return false;
     }
 }
