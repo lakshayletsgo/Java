d206daea2941704e399d4638f0977e22d368cc3b
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-11 13:32:43 +0530

    PracticeReLeetCode lru-cache

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -716,18 +716,68 @@ public class PracticeReLeetCode {
 //    }
 
 
-    public int findDuplicate(int[] nums) {
-        int fast = nums[0];
-        int slow =  nums[0];
-        do{
-            fast= nums[nums[fast]];
-            slow = nums[slow];
-        }while(slow!=fast);
-        fast=nums[0];
-        while(fast!=slow){
-            fast= nums[fast];
-            slow= nums[slow];
+//    public int findDuplicate(int[] nums) {
+//        int fast = nums[0];
+//        int slow =  nums[0];
+//        do{
+//            fast= nums[nums[fast]];
+//            slow = nums[slow];
+//        }while(slow!=fast);
+//        fast=nums[0];
+//        while(fast!=slow){
+//            fast= nums[fast];
+//            slow= nums[slow];
+//        }
+//        return fast;
+//    }
+
+
+    class LRUCache {
+
+        private class Node{
+            Node prev, next;
+            int key, val;
+            Node(int key, int val){
+                this.key = key;
+                this.val= val;
+            }
         }
-        return fast;
+        int capacity;
+        Node head= new Node(0,0),tail = new Node(0,0);
+        Map<Integer,Node> map = new HashMap<>();
+        public LRUCache(int capacity) {
+            this.capacity=capacity;
+            head.next= tail;
+            tail.prev= head;
+        }
+
+        public int get(int key) {
+            if(map.containsKey(key)){
+                Node data= map.get(key);
+                remove(data);
+                insert(data);
+                return data.val;
+            }
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            if(map.containsKey(key))remove(map.get(key));
+            if(map.size()==capacity)remove(tail.prev);
+            insert(new Node(key,value));
+        }
+        void insert(Node node){
+            map.put(node.key,node);
+            Node headNext = head.next;
+            head.next = node;
+            node.prev = head;
+            headNext.prev= node;
+            node.next = headNext;
+        }
+        void remove(Node node){
+            map.remove(node.key);
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
     }
 }
