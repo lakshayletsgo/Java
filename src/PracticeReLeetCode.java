a72e64c295021f1fc2dc108ab9c2cfe32921dbf8
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-19 18:30:47 +0530

    PracticeReLeetCode subtree-of-another-tree

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -48,11 +48,30 @@ public class PracticeReLeetCode {
 
 
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p==null||q==null){
-            return p==q;
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if (p==null||q==null){
+//            return p==q;
+//        }
+//        return p.val==q.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+//    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
         }
-        return p.val==q.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        if (root.val==subRoot.val&&sameTree(root,subRoot)){
+            return true;
+        }
+        return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+    }
+    private boolean sameTree(TreeNode root,TreeNode subRoot){
+        if (root==null&&subRoot==null){
+            return true;
+        }
+        if (root==null||subRoot==null||root.val!=subRoot.val){
+            return false;
+        }
+        return sameTree(root.left,subRoot.left)&&sameTree(root.right,subRoot.right);
     }
 }
 
