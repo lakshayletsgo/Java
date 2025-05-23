a9b94c45b0e8a5d22519091437a6f207d2e1da9e
lakshay <lakshay22csu100@ncuindia.edu>
2025-05-23 12:51:46 +0530

    ReLeetCode_54 lowest-common-ancestor-of-a-binary-tree

--- src/ReLeetCode_54.java
+++ src/ReLeetCode_54.java
@@ -6,12 +6,10 @@ public class ReLeetCode_54 {
 
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.val<p.val&&root.val<q.val){
-            return lowestCommonAncestor(root.right,p,q);
-        } else if (root.val > p.val && root.val > q.val) {
-            return lowestCommonAncestor(root.left,p,q);
-        }else{
-            return root;
-        }
+        if(root==null||root==p||root==q)return root;
+        TreeNode left= lowestCommonAncestor(root.left,p,q);
+        TreeNode right= lowestCommonAncestor(root.right,p,q);
+        if(left!=null&&right!=null)return root;
+        return left==null?right:left;
     }
 }
