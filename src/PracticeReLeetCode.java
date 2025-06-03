441642468063a2f0b4567739946700bef66d1ef6
lakshay <lakshay22csu100@ncuindia.edu>
2025-06-03 11:57:15 +0530

    PracticeReLeetCode reverse-odd-levels-of-binary-tree

--- src/PracticeReLeetCode.java
+++ src/PracticeReLeetCode.java
@@ -295,18 +295,34 @@ public class PracticeReLeetCode {
 
 
 
-    int sum = 0;
-    public TreeNode bstToGst(TreeNode root) {
-        if(root!=null){
-            bstToGst(root.right);
-            sum+=root.val;
-            root.val = sum;
-            bstToGst(root.left);
-        }
+//    int sum = 0;
+//    public TreeNode bstToGst(TreeNode root) {
+//        if(root!=null){
+//            bstToGst(root.right);
+//            sum+=root.val;
+//            root.val = sum;
+//            bstToGst(root.left);
+//        }
+//        return root;
+//    }
+
+
+
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if(root==null)return root;
+        helper(root.left, root.right, 0);
         return root;
     }
-
-
+    private void helper(TreeNode left, TreeNode right, int level){
+        if(left==null||right==null)return ;
+        if(level%2==0){
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+        helper(left.left, right.right, level+1);
+        helper(left.right,right.left, level+1);
+    }
 
 
 }
