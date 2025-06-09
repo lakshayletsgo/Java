a1c2a6c7031da9260afcd078b8adfed37b2631c3
lakshay <lakshay22csu100@ncuindia.edu>
2025-06-09 12:53:58 +0530

    ReLeetCode_70 construct-binary-search-tree-from-preorder-traversal

--- src/ReLeetCode_70.java
+++ src/ReLeetCode_70.java
@@ -5,18 +5,16 @@ public class ReLeetCode_70 {
     public static void main(String[] args) {
 
     }
+
     public TreeNode bstFromPreorder(int[] preorder) {
         TreeNode root = null;
-        for (int i:preorder){
-            root = insertNode(root,i);
-        }
+        for(int i:preorder)root=insertNode(root,i);
         return root;
     }
-
-    private TreeNode insertNode(TreeNode root, int i) {
-        if (root==null)return null;
-        if (root.val<i) root.left = insertNode(root.left,i);
-        if (root.val>i) root.right = insertNode(root.right,i);
+    private TreeNode insertNode(TreeNode root,int i){
+        if(root==null)return new TreeNode(i);
+        if(root.val>i)root.left = insertNode(root.left,i);
+        if(root.val<i)root.right = insertNode(root.right,i);
         return root;
     }
 }
