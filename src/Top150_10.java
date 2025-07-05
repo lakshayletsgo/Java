c07881cca14d1d4c89ee97610596d68d326bcbad
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-05 12:37:41 +0530

    h-index

---
+++ src/Top150_10.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Top150_10 {
+
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i=0;i<n;i++)if(citations[i]>=n-i)return n-i;
+        return 0;
+    }
+}
