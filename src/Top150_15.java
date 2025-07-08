d53f649aea679ae6d7b225a175aa69eebd59b64d
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-08 15:47:24 +0530

    candy

--- src/Top150_15.java
+++ src/Top150_15.java
@@ -5,7 +5,7 @@ public class Top150_15 {
         for(int i=0;i<ratings.length;i++)candles[i]=1;
         for(int i=1;i<ratings.length;i++)if(ratings[i]>ratings[i-1])candles[i] = candles[i-1]+1;
         for(int i=ratings.length-1;i>0;i--){
-            if(ratings[i-1]>ratings[i]) if(ratings[i-1]>ratings[i]) candles[i-1] = Math.max(candles[i]+1,candles[i-1]);
+             if(ratings[i-1]>ratings[i]) candles[i-1] = Math.max(candles[i]+1,candles[i-1]);
             initial+=candles[i-1];
         }
         return initial+candles[ratings.length-1];
