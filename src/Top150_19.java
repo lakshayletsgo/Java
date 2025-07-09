1db541a8ee4de9711311ca47822b91507621c8d6
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-09 22:33:35 +0530

    length-of-last-word

---
+++ src/Top150_19.java
@@ -0,0 +1,16 @@
+public class Top150_19 {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;
+    }
+}
