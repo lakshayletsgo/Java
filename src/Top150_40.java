b875ac09bc8b83b42199f7a55b55ef372d146c4b
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-18 22:20:00 +0530

    isomorphic-strings

---
+++ src/Top150_40.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Top150_40 {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i))!=t.charAt(i)) return false;
+            }else{
+                if(set.contains(t.charAt(i)))return false;
+                map.put(s.charAt(i),t.charAt(i));
+                set.add(t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
