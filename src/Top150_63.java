644eef45db355fa07c667c89ca346a849c8b7f72
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-26 22:59:58 +0530

    remove-nth-node-from-end-of-list

---
+++ src/Top150_63.java
@@ -0,0 +1,16 @@
+public class Top150_63 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        ListNode temp2 = head;
+        ListNode prev = null;
+        for(int i=1;i<=n;i++)temp = temp.next;
+        if(temp==null)return head.next;
+        while(temp!=null){
+            prev = temp2;
+            temp = temp.next;
+            temp2 = temp2.next;
+        }
+        prev.next = temp2.next;
+        return head;
+    }
+}
