a8f3c3a4c619edd2f10381f5cf9df5856cc4de94
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-31 23:06:29 +0530

    sum-root-to-leaf-numbers

---
+++ src/Top150_77.java
@@ -0,0 +1,13 @@
+public class Top150_77 {
+        public int sumNumbers(TreeNode root) {
+                return helper(root,0);
+        }
+        public int helper(TreeNode root, int sum){
+                if(root==null)return 0;
+                if(root.left==null&&root.right==null)return sum*10+root.val;
+                int left = helper(root.left,sum*10+root.val);
+                int right = helper(root.right,sum*10+root.val);
+                return left+right;
+
+        }
+}
