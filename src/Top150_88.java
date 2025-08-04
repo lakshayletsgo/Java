2a4fd4769da761ec884268b06a6e68ec181805f3
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-04 13:49:32 +0530

    validate-binary-search-tree

---
+++ src/Top150_88.java
@@ -0,0 +1,11 @@
+public class Top150_88 {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    private boolean helper(TreeNode root,long min,long max){
+        if(root==null)return true;
+        if(root.val<=min||root.val>=max)return false;
+        return helper(root.left,min,root.val)&&helper(root.right,root.val,max);
+
+    }
+}
