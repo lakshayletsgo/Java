546d075049a076e904eb0a9baba9fd2e775c73a6
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-18 14:40:46 +0530

    search-in-rotated-sorted-array

---
+++ src/Top150_117.java
@@ -0,0 +1,18 @@
+public class Top150_117 {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[start]<=nums[mid]){
+                if(nums[start]<=target&&target<nums[mid])end = mid-1;
+                else start = mid+1;
+            }else{
+                if(nums[mid]<target&&target<=nums[end])start = mid+1;
+                else end = mid-1;
+            }
+        }
+
+        return -1;
+    }
+}
