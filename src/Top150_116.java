c0f1d83067c23972e0b534bf777e6f8519f4088a
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-18 14:20:34 +0530

    find-peak-element

---
+++ src/Top150_116.java
@@ -0,0 +1,11 @@
+public class Top150_116 {
+    public int findPeakElement(int[] nums) {
+        int start = 0,end = nums.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]>nums[mid+1])end = mid;
+            else start = mid+1;
+        }
+        return start;
+    }
+}
