99c7ea6bd961c1e14a1d303423668e5841d6c2b3
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-19 11:54:26 +0530

    find-minimum-in-rotated-sorted-array

---
+++ src/Top150_119.java
@@ -0,0 +1,12 @@
+public class Top150_119 {
+    public int findMin(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(nums[mid]>nums[right])left=mid+1;
+            else right=mid;
+        }
+        return nums[left];
+    }
+}
