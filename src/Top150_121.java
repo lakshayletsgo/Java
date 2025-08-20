8339c38fde70d914a05ef578821d5e7f0cec8f2a
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-20 15:36:53 +0530

    kth-largest-element-in-an-array

---
+++ src/Top150_121.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Top150_121 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
