7af757292e19d7c3e0e0360bf8de35b2ae52d879
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-24 14:02:43 +0530

    single-number-ii

---
+++ src/Top150_129.java
@@ -0,0 +1,10 @@
+public class Top150_129 {
+    public int singleNumber(int[] nums) {
+        int one =0,two =0;
+        for(int i=0;i<nums.length;i++){
+            one = (one^nums[i])& ~two;
+            two = (two^nums[i])& ~one;
+        }
+        return one;
+    }
+}
