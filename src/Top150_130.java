f85a6d92b3fe14cf886103eb50cf699487fd3597
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-24 14:49:21 +0530

    bitwise-and-of-numbers-range

---
+++ src/Top150_130.java
@@ -0,0 +1,12 @@
+public class Top150_130 {
+    public int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+        while(left!=right){
+            left>>=1;
+            right>>=1;
+            count++;
+        }
+        return (left<<count);
+
+    }
+}
