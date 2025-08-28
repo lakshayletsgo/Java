e0b5412139e918a939247ab1e96db259c09e2579
lakshay <lakshay22csu100@ncuindia.edu>
2025-08-28 22:17:32 +0530

    coin-change

---
+++ src/Top150_140.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Top150_140 {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[]dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int coin:coins)for(int i=coin;i<=amount;i++)dp[i] = Math.min(dp[i],dp[i-coin]+1);
+
+        return dp[amount]>amount?-1:dp[amount];
+    }
+}
