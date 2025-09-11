a344c7bb3d2c1db5b6f4cc4902689ff473e82a17
lakshay <lakshay22csu100@ncuindia.edu>
2025-09-11 17:50:43 +0530

    remove-duplicates-from-sorted-array

---
+++ src/Striver_16.java
@@ -0,0 +1,12 @@
+public class Striver_16 {
+    public int removeDuplicates(int[] nums) {
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i-1]){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
