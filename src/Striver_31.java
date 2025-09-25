28c4c018a461cb8d12df2e61195d8eaf84e63172
lakshay <lakshay22csu100@ncuindia.edu>
2025-09-25 23:25:56 +0530

    merge-intervals

---
+++ src/Striver_31.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Striver_31 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int start = 0;
+        int end = 0;
+        int[]prev = intervals[0];
+        List<int[]> list = new ArrayList<>();
+        for(int[]interval:intervals){
+            if(interval[0]<=prev[1]){
+                prev[1]= Math.max(interval[1],prev[1]);
+            }else{
+                list.add(prev);
+                prev = interval;
+            }
+        }
+        list.add(prev);
+        return list.toArray(new int[list.size()][]);
+
+    }
+}
